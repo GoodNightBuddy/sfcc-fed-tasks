a6672c55839cd921b3107bfcd4e9e181ec1f5901
Pavlo Navrotskyi <pnavrotskyi@speroteck.com>
2022-09-28 10:59:02 +0300

    Added controllers

---
+++ cartridges/site_pnavrotskyi/cartridge/controllers/Hello.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var server = require('server');
+var cache = require('*/cartridge/scripts/middleware/cache');
+
+
+server.get('Start', cache.applyDefaultCache, function (req, res, next) {
+    var Site = require('dw/system/Site');
+
+    res.render('hello/helloWorld', { param1: Site.current.name });
+    // res.json({ siteCurrentName: Site.current.name });
+    next();
+});
+
+module.exports = server.exports();
