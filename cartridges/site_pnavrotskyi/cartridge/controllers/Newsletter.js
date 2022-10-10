e49e92aa777df64cb4ff8c91662222257eb65cbb
Pavlo Navrotskyi <pnavrotskyi@speroteck.com>
2022-10-10 12:17:57 +0300

    Realize custom objects and hooks task

--- cartridges/site_pnavrotskyi/cartridge/controllers/Newsletter.js
+++ cartridges/site_pnavrotskyi/cartridge/controllers/Newsletter.js
@@ -3,6 +3,7 @@
 var server = require('server');
 var URLUtils = require('dw/web/URLUtils');
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var Resource = require('dw/web/Resource');
 
 server.get('Show', csrfProtection.generateToken, function (req, res, next) {
     var actionUrl = dw.web.URLUtils.url('Newsletter-Handler');
@@ -19,19 +20,45 @@ server.get('Show', csrfProtection.generateToken, function (req, res, next) {
 
 server.post('Handler', csrfProtection.validateAjaxRequest, function (req, res, next) {
     var newsletterForm = server.forms.getForm('newsletter');
-
+    var txn = require('dw/system/Transaction');
     newsletterForm.valid = newsletterForm.email.value === newsletterForm.emailconfirm.value && newsletterForm.valid;
 
     if (newsletterForm.valid) {
-        res.json({
-            success: true,
-            redirectUrl: URLUtils.url('Newsletter-Success').toString()
-        });
+        try {
+            txn.wrap(function () {
+                var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+                var co = CustomObjectMgr.createCustomObject('NewsletterSubscriptionPNavrotskyi', newsletterForm.email.value);
+                co.custom.firstName = newsletterForm.fname.value;
+                co.custom.lastName = newsletterForm.lname.value;
+
+                res.json({
+                    success: true,
+                    redirectUrl: URLUtils.url('Newsletter-Success').toString()
+                });
+
+                dw.system.HookMgr.callHook('newsletter.email', 'send', newsletterForm.email.value);
+            });
+        } catch (err) {
+            if (err.javaName === 'MetaDataException') {
+                res.json({
+                    success: false,
+                    error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
+                });
+            } else {
+                var Logger = require('dw/system/Logger');
+                Logger.getLogger('newsletter_subscription').error(Resource.msg('error.customobjectmissing', 'newsletter', null));
+
+                res.setStatusCode(500);
+                res.json({
+                    error: true,
+                    redirectUrl: URLUtils.url('Error-Start').toString()
+                });
+            }
+        }
     } else {
-        res.setStatusCode(500);
         res.json({
-            error: true,
-            redirectUrl: URLUtils.url('Error-Start').toString()
+            success: false,
+            error: [Resource.msg('error.crossfieldvalidation', 'newsletter', null)]
         });
     }
 
@@ -49,6 +76,5 @@ server.get('Success', function (req, res, next) {
 
     next();
 });
-
 
 module.exports = server.exports();
