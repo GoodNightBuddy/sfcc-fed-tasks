1e3a8752b437767ba34ec83abee0a7c92a1e64c4
Pavlo Navrotskyi <pnavrotskyi@speroteck.com>
2022-10-31 12:14:14 +0200

    Remove temlate

--- cartridges/site_pnavrotskyi/cartridge/scripts/hooks/orderHooks.js
+++ cartridges/site_pnavrotskyi/cartridge/scripts/hooks/orderHooks.js
@@ -1,42 +1,48 @@
+var Mail = require('dw/net/Mail');
 var Logger = require('dw/system/Logger');
 var Status = require('dw/system/Status');
+var Site = require('dw/system/Site').getCurrent();
 var Transaction = require('dw/system/Transaction');
 var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+var collections = require('*/cartridge/scripts/util/collections');
 
 function beforePATCH(order, orderInput) {
     try {
         if (+orderInput.c_PNavrotskyiStatus === 1) {
             var email = order.getCustomerEmail();
             if (email) {
-                var OrderModel = require('*/cartridge/models/order');
-                var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
+                var products = order.getProductLineItems();
+                var productsInfo = 'Your order contains: \n';
 
-                var config = {
-                    numberOfLineItems: '*'
-                };
-
-                var orderModel = new OrderModel(
-                    order,
-                    { config: config, containerView: 'order' }
-                );
-
-
-                var emailParams = {
-                    to: email,
-                    subject: 'Canceled order'
-                };
-                emailHelpers.send(emailParams, 'order/orderDetails', {
-                    order: orderModel
+                collections.forEach(products, function (product, i) {
+                    var separator = ',\n';
+                    if (i === products.length - 1) {
+                        separator = '.';
+                    }
+                    productsInfo += product.getProductName() + ' in quantity: ' + product.quantityValue + separator;
                 });
+
+                var orderInfo = productsInfo + '\nOrder information:' +
+                '\nCreation Date: ' + order.getCreationDate() +
+                '\nTotal Tax: ' + order.getTotalTax() + '$' +
+                '\nTotal Shipping Cost: ' + order.getShippingTotalPrice() + '$' +
+                '\nTotal Price: ' + order.getTotalGrossPrice() + '$';
+
+                var mail = new Mail();
+                mail.addTo(order.getCustomerEmail());
+                mail.setFrom(Site.current.getCustomPreferenceValue('customerServiceEmail'));
+                mail.setSubject('Canceled order information');
+                mail.setContent(orderInfo);
+                mail.send();
 
             }
         }
+        return new Status(Status.OK);
     } catch (error) {
         Logger.getLogger('orderHook_beforePATCH').error(error.toString());
         return new Status(Status.ERROR, error.toString());
     }
 
-    return new Status(Status.OK);
 }
 
 function afterPATCH(order, orderInput) {
@@ -45,18 +51,17 @@ function afterPATCH(order, orderInput) {
             Transaction.wrap(function () {
                 var co = CustomObjectMgr.createCustomObject('PNavrotskyiRefunds', order.UUID);
                 co.custom.orderNo = order.getOrderNo();
-                co.custom.totalRefund = +order.getTotalGrossPrice();
                 co.custom.paymentToken = order.getOrderToken();
                 co.custom.customerNumber = order.getCustomerNo();
+                co.custom.totalRefund = +order.getTotalGrossPrice();
             });
         }
-
+        return new Status(Status.OK);
     } catch (error) {
         Logger.getLogger('orderHook_beforePATCH').error(error.toString());
         return new Status(Status.ERROR, error.toString());
     }
 
-    return new Status(Status.OK);
 }
 
 exports.beforePATCH = beforePATCH;
