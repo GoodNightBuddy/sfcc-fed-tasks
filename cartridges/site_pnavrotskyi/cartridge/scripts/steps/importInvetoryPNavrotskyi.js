ac69bd0e565e69539c5da8b309b75208b5fecf9a
Pavlo Navrotskyi <pnavrotskyi@speroteck.com>
2022-11-19 12:12:20 +0200

    Added error handling

--- cartridges/site_pnavrotskyi/cartridge/scripts/steps/importInvetoryPNavrotskyi.js
+++ cartridges/site_pnavrotskyi/cartridge/scripts/steps/importInvetoryPNavrotskyi.js
@@ -2,13 +2,47 @@
 /* eslint-disable no-continue */
 'use strict';
 
-var ProductMgr = require('dw/catalog/ProductMgr');
 var File = require('dw/io/File');
+var system = require('dw/system');
 var Logger = require('dw/system/Logger');
 var FileWriter = require('dw/io/FileWriter');
 var FileReader = require('dw/io/FileReader');
+var ProductMgr = require('dw/catalog/ProductMgr');
 var CSVStreamReader = require('dw/io/CSVStreamReader');
 var XMLIndentingStreamWriter = require('dw/io/XMLIndentingStreamWriter');
+
+const DirectoryNotFoundError = 'DirectoryNotFoundError';
+const DirectoryIsEmptyError = 'DirectoryIsEmptyError';
+const FileNotFoundError = 'FileNotFoundError';
+
+function getFile(sourceFolderPath, regExp) {
+    var sourceFolder = new File(sourceFolderPath);
+    var filesList = sourceFolder.listFiles();
+    var sourceFileName;
+
+    if (!filesList) {
+        throw DirectoryNotFoundError;
+    }
+
+    if (!filesList.length) {
+        throw DirectoryIsEmptyError;
+    }
+
+    for (var i = 0; i < filesList.length; i++) {
+        if (regExp.test(filesList[i].name)) {
+            sourceFileName = filesList[i].name;
+            break;
+        }
+    }
+
+    if (!sourceFileName) {
+        throw FileNotFoundError;
+    }
+
+    var sourceFilePath = sourceFolderPath + sourceFileName;
+    var sourceFile = new File(sourceFilePath);
+    return sourceFile;
+}
 
 function startInvetoryList(xsw, lisId) {
     xsw.writeStartElement('inventory-list');
@@ -53,23 +87,6 @@ function createRecord(xsw, productId, allocation) {
         xsw.writeEndElement();
 }
 
-function getFile(sourceFolderPath, regExp) {
-    var sourceFolder = new File(sourceFolderPath);
-    var filesList = sourceFolder.listFiles();
-    var sourceFileName;
-
-    for (var i = 0; i < filesList.length; i++) {
-        if (regExp.test(filesList[i].name)) {
-            sourceFileName = filesList[i].name;
-            break;
-        }
-    }
-
-    var sourceFilePath = sourceFolderPath + sourceFileName;
-    var sourceFile = new File(sourceFilePath);
-    return sourceFile;
-}
-
 function startDocument(xsw) {
     xsw.setIndent('    ');
     xsw.writeStartDocument();
@@ -86,70 +103,81 @@ function endDocument(xsw) {
 }
 
 function importInvetory(parameters) {
-    var regExp = new RegExp(parameters.FileNamePattern);
-    var paramsPath = parameters.SourceFolderPath || '';
-    if (paramsPath) {
-        paramsPath = paramsPath.split('\\').join(File.SEPARATOR); // if someone by mistake entered wrong slash
-        paramsPath = paramsPath.split('/').join(File.SEPARATOR) + File.SEPARATOR;
-    }
-    var sourceFolderPath = File.IMPEX + File.SEPARATOR + 'src' + File.SEPARATOR + paramsPath;
-    var prefix = parameters.Prefix + '_';
-    var sourceFile = getFile(sourceFolderPath, regExp);
+    try {
+        var regExp = new RegExp(parameters.FileNamePattern);
+        var paramsPath = parameters.SourceFolderPath || '';
+        if (paramsPath) {
+            paramsPath = paramsPath.split('\\').join(File.SEPARATOR); // if someone by mistake entered wrong slash
+            paramsPath = paramsPath.split('/').join(File.SEPARATOR) + File.SEPARATOR;
+        }
+        var sourceFolderPath = File.IMPEX + File.SEPARATOR + 'src' + File.SEPARATOR + paramsPath;
+        var prefix = parameters.Prefix + '_';
+        var sourceFile = getFile(sourceFolderPath, regExp);
 
-    if (!sourceFile) return;
+        var fileseparator = ',';
+        var fileReader = new FileReader(sourceFile, 'UTF-8');
+        var csr = new CSVStreamReader(fileReader, fileseparator);
 
-    var fileseparator = ',';
-    var fileReader = new FileReader(sourceFile, 'UTF-8');
-    var csr = new CSVStreamReader(fileReader, fileseparator);
+        var listFileName = 'inventoryListPNavrotskyi.xml';
+        var listFilePath = File.IMPEX + File.SEPARATOR + 'src' + File.SEPARATOR + 'inventoryPNavrotskyi' + File.SEPARATOR + listFileName;
+        var listFile = new File(listFilePath);
+        var fileWriter = new FileWriter(listFile, 'UTF-8');
+        var xsw = new XMLIndentingStreamWriter(fileWriter);
 
-    var listFileName = 'inventoryListPNavrotskyi.xml';
-    var listFilePath = File.IMPEX + File.SEPARATOR + 'src' + File.SEPARATOR + 'inventoryPNavrotskyi' + File.SEPARATOR + listFileName;
-    var listFile = new File(listFilePath);
-    var fileWriter = new FileWriter(listFile, 'UTF-8');
-    var xsw = new XMLIndentingStreamWriter(fileWriter);
+        startDocument(xsw);
 
-    startDocument(xsw);
+        var listCount = 0;
+        var row = csr.readNext();
+        while (row) {
+            if (row.some(function (el) { return isNaN(+el); })) {
+                row = csr.readNext();
+                continue;
+            }
 
-    var listCount = 0;
-    var row = csr.readNext();
-    while (row) {
-        if (row.some(function (el) { return isNaN(+el); })) {
+            var listNumber = +row[0];
+            var productId = row[1];
+            var allocation = row[2];
+
+            if (!ProductMgr.getProduct(productId)) {
+                row = csr.readNext();
+                continue;
+            }
+
+            if (listCount === 0) {
+                startInvetoryList(xsw, prefix + listNumber);
+                listCount = listNumber;
+            }
+
+            if (listNumber !== listCount) {
+                endInvetoryList(xsw);
+                startInvetoryList(xsw, prefix + listNumber);
+                listCount++;
+            }
+
+            createRecord(xsw, productId, allocation);
+
             row = csr.readNext();
-            continue;
         }
 
-        var listNumber = +row[0];
-        var productId = row[1];
-        var allocation = row[2];
+        endDocument(xsw);
 
-        if (!ProductMgr.getProduct(productId)) {
-            row = csr.readNext();
-            continue;
+        csr.close();
+        xsw.close();
+        fileReader.close();
+        fileWriter.close();
+        // sourceFile.remove();
+    } catch (error) {
+        if (error === DirectoryNotFoundError) {
+            return new system.Status(system.Status.ERROR, null,'Provided directory not found!');
         }
-
-        if (listCount === 0) {
-            startInvetoryList(xsw, prefix + listNumber);
-            listCount = listNumber;
+        if (error === DirectoryNotFoundError) {
+            return new system.Status(system.Status.ERROR, null,'Provided directory not found!');
         }
-
-        if (listNumber !== listCount) {
-            endInvetoryList(xsw);
-            startInvetoryList(xsw, prefix + listNumber);
-            listCount++;
+        if (error === DirectoryNotFoundError) {
+            return new system.Status(system.Status.ERROR, null,'Provided directory not found!');
         }
-
-        createRecord(xsw, productId, allocation);
-
-        row = csr.readNext();
+        return new system.Status(system.Status.ERROR, null, error.message);
     }
-
-    endDocument(xsw);
-
-    csr.close();
-    xsw.close();
-    fileReader.close();
-    fileWriter.close();
-    // sourceFile.remove();
 }
 
 module.exports = {
