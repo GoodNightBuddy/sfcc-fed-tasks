4fcebb9c1944b11cc1135270833dbc2a41b4c921
GoodNightBuddy <fun.love.nuts@gmail.com>
2023-04-26 16:14:39 +0300

    Third task

---
+++ cartridges/site_pnavrotskyi/cartridge/client/default/js/homeSlickSlider/homeSlickSlider.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    slickSliderCat: function () {
+        $('.slick-carousel:not(.slick-initialized)').slick({
+            dots: true,
+            arrows: true,
+            prevArrow: '<a class="slick-prev d-flex justify-content-center align-items-center"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>',
+            nextArrow: '<a class="slick-next d-flex justify-content-center align-items-center"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>',
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 5000
+        });
+    }
+};
+
